442ab75cab6c92b63a8d737391bdcff4cdfdafe3
Daniel Emidio <104872988+DanEmidio@users.noreply.github.com>
2026-03-08 12:37:02 -0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,57 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+const form = document.getElementById("formManutencao")
+
+if(form){
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault()
+
+let registro = {
+
+trem: document.getElementById("trem").value,
+carro: document.getElementById("carro").value,
+equipamento: document.getElementById("equipamento").value,
+tipo: document.getElementById("tipo").value,
+tecnico: document.getElementById("tecnico").value
+
+}
+
+let historico = JSON.parse(localStorage.getItem("manutencoes")) || []
+
+historico.push(registro)
+
+localStorage.setItem("manutencoes", JSON.stringify(historico))
+
+alert("Manutenção registrada")
+
+})
+
+}
+
+let tabela = document.getElementById("tabelaHistorico")
+
+if(tabela){
+
+let historico = JSON.parse(localStorage.getItem("manutencoes")) || []
+
+historico.forEach(reg => {
+
+let linha = `<tr>
+
+<td>${reg.trem}</td>
+<td>${reg.carro}</td>
+<td>${reg.equipamento}</td>
+<td>${reg.tipo}</td>
+<td>${reg.tecnico}</td>
+
+</tr>`
+
+tabela.innerHTML += linha
+
+})
+
+}
+
+})
